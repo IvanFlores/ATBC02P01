e49b10a4d26b43db8e508a11d310dc627cf0c67d
Juan Gutierrez <juandgutierr@gmail.com>
2021-02-22 12:53:33 -0400

    commenting python command class example

--- endpoints/core/__init__.py
+++ endpoints/core/__init__.py
@@ -1,6 +1,8 @@
-from endpoints.core.python_command import PythonCommand
-
 # test if python command works (should be erased)
-python_command = PythonCommand(r'D:\Juan\programas\python', r'C:\Users\Public', r'\command')
-command = python_command.builder()
-python_command.executer(command)
+
+# from endpoints.core.python_command import PythonCommand
+
+
+# python_command = PythonCommand(r'D:\Juan\programas\python', r'C:\Users\Public', r'\command')
+# command = python_command.builder()
+# python_command.executer(command)
